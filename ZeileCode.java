757e1f8f41062a2f4356c46385901aa07ca41506
Karina <karin.michlczyk@gmail.com>
2020-08-12 11:23:22 +0200

    first file done

---
+++ ZeileCode.java
@@ -0,0 +1,16 @@
+
+public class ZeileCode {
+
+	public static void main(String[] args) {
+		
+		double x = 15.5;  
+		double g = ((((Math.sqrt(3.5 +x))*5)/3)-((x+10)*(x-4.1)));
+		
+		System.out.println (g);
+	
+	
+		
+		 
+	}
+
+}
